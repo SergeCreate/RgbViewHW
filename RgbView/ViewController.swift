210013578df8d844b5522692f063cd0f561351c1
Sergey Zakharov <sergey_sentybr@mail.ru>
2023-03-25 22:42:31 +0200

    viewController finished

--- RgbView/ViewController.swift
+++ RgbView/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var coloredView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
+    
+    private func changeColor() {
+        coloredView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value), alpha: 1
+        )
+    }
+    
 
-
+    @IBAction func redSlider(_ sender: Any) {
+        changeColor()
+        redLabel.text = redSlider.value.formatted(.number.precision(.fractionLength(2)))
+    }
+    
+    @IBAction func greenSlider(_ sender: Any) {
+        changeColor()
+        greenLabel.text = greenSlider.value.formatted(.number.precision(.fractionLength(2)))
+    }
+    
+    @IBAction func blueSlider(_ sender: Any) {
+        changeColor()
+        blueLabel.text = blueSlider.value.formatted(.number.precision(.fractionLength(2)))
+    }
 }
 
